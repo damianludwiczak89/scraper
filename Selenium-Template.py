725a88c83d7635d92dfa4cb8602583b2e228b461
damianludwiczak89 <103378303+damianludwiczak89@users.noreply.github.com>
2025-01-20 14:34:43 +0100

    Update Selenium-Template.py
    update

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
+from bs4 import BeautifulSoup
+
 display = Display(visible=0, size=(800, 800))  
 display.start()
 
@@ -33,8 +35,14 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
-print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+driver.get('https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-asus-vivobook-15x-k3504va-ma480w-15-6-oled-i7-1355u-16gb-ram-1tb-ssd-windows-11-home')
+html = driver.page_source
+
+driver.quit
+
+page = BeautifulSoup(html, 'html.parser')
+
+price = page.find('span', class_='whole')
+
+print(price.text)
 
