74eec4cd479e10138dfe0013333a63bdbc9f9519
damianludwiczak89 <103378303+damianludwiczak89@users.noreply.github.com>
2025-01-20 14:52:09 +0100

    Update Selenium-Template.py
    email config

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -5,6 +5,10 @@ import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 from bs4 import BeautifulSoup
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 display = Display(visible=0, size=(800, 800))  
 display.start()
 
@@ -32,17 +36,35 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
+sender_email = "damian_konin@tlen.pl"
+receiver_email = "damian_konin@tlen.pl"
+password = os.getenv("EMAIL_PASSWORD")
+
     
 driver = webdriver.Chrome(options = chrome_options)
-
 driver.get('https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-asus-vivobook-15x-k3504va-ma480w-15-6-oled-i7-1355u-16gb-ram-1tb-ssd-windows-11-home')
 html = driver.page_source
-
 driver.quit
-
 page = BeautifulSoup(html, 'html.parser')
-
 price = page.find('span', class_='whole')
 
-print(price.text)
+price = int(price.text.replace(' ', ''))
 
+if price < 4199:
+  message['Subject'] = f'Cena Laptopa spadła! Nowa cena: f{price}!'
+else:
+  message['Subject'] = f'Cena nie spadła. Aktualna cena {price}'
+
+body = "https://www.mediaexpert.pl/komputery-i-tablety/laptopy-i-ultrabooki/laptopy/laptop-asus-vivobook-15x-k3504va-ma480w-15-6-oled-i7-1355u-16gb-ram-1tb-ssd-windows-11-home"
+
+message.attach(MIMEText(body, 'plain'))
+
+try:
+    with smtplib.SMTP('poczta.o2.pl', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        text = message.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        print("Email sent successfully!")
+except Exception as e:
+    print(f"Failed to send email. Error: {e}")
