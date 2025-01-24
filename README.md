962fac2ae97ce7a165a64497c1e104ddabe7b159
damianludwiczak89 <damian_konin@tlen.pl>
2025-01-24 16:40:36 +0100

    second scraper

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is just a scraper to check price of laptop daily using GitHub Actions. Had to use Selenium because simple request did not gather data correctly from the site.
 
+check_price2.py added to check a price of other item for my girlfriend.
+
 Credits to [MarketingPipeline](https://github.com/MarketingPipeline) for creating and sharing a config template
 
 ## License ![GitHub](https://img.shields.io/github/license/MarketingPipeline/Python-Selenium-Action)
---
+++ check_price2.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import os
+
+sender_email = "damian_konin@tlen.pl"
+receiver_email = "olga_m922@tlen.pl"
+password = os.getenv("EMAIL_PASSWORD")
+
+url = 'https://www.zooplus.pl/shop/koty/drapaki_dla_kota/drapak_maly/100_cm/49253?activeVariant=49253.0'
+result = requests.get(url)
+page = BeautifulSoup(result.text, 'html.parser')
+price = page.find('span', class_='z-price__amount z-price__amount--standard').text
+price = float(price[:-3].replace(',', '.'))
+
+message = MIMEMultipart()
+message['From'] = sender_email
+message['To'] = receiver_email
+
+if price < 109.99:
+  message['Subject'] = f'CENA SŁUPKA SPADŁA!!!! Nowa cena: {price} zł!'
+else:
+  message['Subject'] = f'Cena słupka nie zmalała. Aktualna cena: {price} zł!'
+
+body = 'https://www.zooplus.pl/shop/koty/drapaki_dla_kota/drapak_maly/100_cm/49253?activeVariant=49253.0'
+
+message.attach(MIMEText(body, 'plain'))
+
+try:
+    with smtplib.SMTP('poczta.o2.pl', 587) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        text = message.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        print("Email sent successfully!")
+except Exception as e:
+    print(f"Failed to send email. Error: {e}")
+
